2974de0f94a9dd5060b42d6a8d97943c90fbc96c
Diana Kazani <114058289+DianaKazani@users.noreply.github.com>
2022-10-17 10:01:24 +0200

    Add files via upload

---
+++ Homework.1/src/main/java/Homework.java
@@ -0,0 +1,39 @@
+public class Homework {
+    public static void main(String[] args) {
+        byte a = 116;
+        System.out.println(a);
+        short b = 1168;
+        System.out.println(b);
+        int c = 2000031;
+        System.out.println(c);
+        long d = 30000091;
+        System.out.println(d);
+
+        byte e = 56;
+        byte f = 16;
+        System.out.println(e+f);
+
+        byte g = 50/3;
+        System.out.println(g);
+
+        int i =2;
+        System.out.println(i*i);
+        int k = 8;
+        Math.pow(8,2);
+        System.out.println(Math.pow(8,2));
+
+        int one = 0;
+        int two = 1;
+        int three = 2;
+        int four = 3;
+        int five = 4;
+        int six = 5;
+        int seven = 6;
+        int eight = 7;
+        int nine = 8;
+        int resalt = one + two+three+four+five+six+seven+eight+nine;
+        System.out.println(resalt);
+         int resalt2 = resalt/10;
+        System.out.println(resalt2);
+    }
+}
